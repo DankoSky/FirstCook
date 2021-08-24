b94101f779e93dfb2b28d2123e5cd9e6e826bdce
DankoSky <CruelDugal@gmail.com>
2021-08-25 03:17:46 +0600

    v3.5

--- src/main/java/com/telegrambots/firstcook/myBot.java
+++ src/main/java/com/telegrambots/firstcook/myBot.java
@@ -15,11 +15,14 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -105,6 +108,27 @@ public class myBot extends TelegramLongPollingBot {
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
+            }
+
+            if (update.hasMessage() && update.getMessage().hasPhoto()) {
+                // Array with photos
+                List<PhotoSize> photos = update.getMessage().getPhoto();
+                // Get largest photo's file_id
+                String f_id = photos.stream()
+                        .max(Comparator.comparing(PhotoSize::getFileSize))
+                        .orElseThrow().getFileId();
+                message.setText(f_id);
+                execute(message);
+                // Send photo by file_id we got before
+//                SendPhoto msg = new SendPhoto()
+//                        .setChatId(chat_id)
+//                        .setPhoto(new InputFile(f_id))
+//                        .setCaption("Photo");
+//                try {
+//                    execute(msg); // Call method to send the photo
+//                } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
             }
 //            if (count % 5 == 0) {
 //                try {
