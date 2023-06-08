d5a12dd2f58c57b6b6cccbc35e8b01424d5876c5
vtsapko <vtsapko@diasoft.ru>
2023-06-08 16:49:27 +0300

    навалил кринжатины

--- src/main/java/com/telegrambots/firstcook/repository/UserPostgreRepository.java
+++ src/main/java/com/telegrambots/firstcook/repository/UserPostgreRepository.java
@@ -1,9 +1,22 @@
 package com.telegrambots.firstcook.repository;
 
+import com.telegrambots.firstcook.model.Role;
 import com.telegrambots.firstcook.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface UserPostgreRepository extends JpaRepository<User, String> {
+public class UserPostgreRepository {
+    public List<User> findAll() {
+        return List.of(User.builder()
+                .birthday(LocalDate.of(2002, 1, 1))
+                .id(0)
+                .chat_id("1")
+                .username("Pezduk usatiy")
+                .isAdmin(Role.USER)
+                .build());
+    }
 }
